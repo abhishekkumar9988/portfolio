d8b4dd0f63356244d8a52126fb6d288413a7e17b
Abhishek kumar <133053701+abhishekkumar9988@users.noreply.github.com>
2024-06-26 23:52:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorALL('section');
+let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
@@ -13,13 +13,17 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active')
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
                
             })
         }
 
     })
 }
+
+
+
+
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
