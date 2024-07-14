8e9cd5d0d82ca9818b84d980bff543b92f477ec7
Abhishek kumar <133053701+abhishekkumar9988@users.noreply.github.com>
2024-07-14 23:57:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
+let sections = document.querySelectorAll('section'); 
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
